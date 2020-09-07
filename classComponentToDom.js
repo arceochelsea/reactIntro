b5e6138b96d6eb59fd943665a36c67b26d14f724
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-09-06 23:01:29 -0400

    Render a Class Component to the DOM

---
+++ classComponentToDom.js
@@ -0,0 +1,23 @@
+// React components are passed into ReactDOM.render() a little differently than JSX elements. For JSX elements, you pass in the name of the element that you want to render. However, for React components, you need to use the same syntax as if you were rendering a nested component, for example ReactDOM.render(<ComponentToRender />, targetNode). You use this syntax for both ES6 class components and functional components.
+
+// Both the Fruits and Vegetables components are defined for you behind the scenes. Render both components as children of the TypesOfFood component, then render TypesOfFood to the DOM. There is a div with id='challenge-node' available for you to use.
+
+class TypesOfFood extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h1>Types of Food:</h1>
+          {/* change code below this line */}
+        <Fruits />
+        <Vegetables />
+          {/* change code above this line */}
+        </div>
+      );
+    }
+  };
+  
+  // change code below this line
+  ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'))
