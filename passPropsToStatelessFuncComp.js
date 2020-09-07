a0a6e4bae2f259253f6274840474e5991e65d5d3
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-09-06 23:58:56 -0400

    Pass Props to a Stateless Functional Component

---
+++ passPropsToStatelessFuncComp.js
@@ -0,0 +1,33 @@
+// You use custom HTML attributes created by you and supported by React to be passed to the component. In this case, the created property user is passed to the component Welcome. Since Welcome is a stateless functional component, it has access to this value like so:
+// const Welcome = (props) => <h1>Hello, {props.user}!</h1>
+
+// It is standard to call this value props and when dealing with stateless functional components, you basically consider it as an argument to a function which returns JSX. You can access the value of the argument in the function body. With class components, you will see this is a little different.
+
+// There are Calendar and CurrentDate components in the code editor. When rendering CurrentDate from the Calendar component, pass in a property of date assigned to the current date from JavaScript's Date object. Then access this prop in the CurrentDate component, showing its value within the p tags. Note that for prop values to be evaluated as JavaScript, they must be enclosed in curly brackets, for instance date={Date()}.
+
+const CurrentDate = (props) => {
+    return (
+      <div>
+        { /* change code below this line */ }
+        <p>The current date is: {props.date} </p>
+        { /* change code above this line */ }
+      </div>
+    );
+  };
+  
+  class Calendar extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h3>What date is it?</h3>
+          { /* change code below this line */ }
+          <CurrentDate date={Date()}/>
+          { /* change code above this line */ }
+        </div>
+      );
+    }
+  };
+  
